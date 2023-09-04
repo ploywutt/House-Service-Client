79f58af7a7d98fd60f008473c0a5fec572d381a8
themeyv <themeyv@gmail.com>
2023-09-04 23:17:10 +0700

    fix: add button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,8 @@
 // import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import faceBookLogo from "../assets/icon/facebook_logos.png";
 
 function UserLogin() {
   // const [input, setInput] = useState<string>();
@@ -41,6 +43,36 @@ function UserLogin() {
             placeholder="Password"
           />
         </div>
+      </div>
+      <div>
+        <Button className="w-96 h-11 px-6 py-2.5 no-underline marker:rounded-lg justify-center items-center gap-2 inline-flex m-8">
+          เข้าสู่ระบบ
+        </Button>
+      </div>
+      <div className="w-96 h-5 justify-center items-center gap-2 inline-flex">
+        <div className="grow shrink basis-0 h-px bg-gray-400" />
+        <div className="text-center text-gray-500 text-sm font-normal leading-tight">
+          หรือลงชื่อเข้าใช้ผ่าน
+        </div>
+        <div className="grow shrink basis-0 h-px bg-gray-400" />
+      </div>
+      <div>
+        <Button
+          variant="secondary"
+          className="w-96 h-11 px-6 py-2.5 no-underline rounded-lg border border-blue-600 text-blue-600  justify-center items-center gap-2 inline-flex m-8"
+        >
+          <img src={faceBookLogo} className="mr-2 h-4 w-4" />
+          เข้าสู่ระบบด้วย Facebook
+        </Button>
+      </div>
+      <div className="text-center text-gray-500 text-base font-normal leading-normal m-4">
+        ยังไม่มีบัญชีผู้ใช้ HomeService?
+        <Button
+          variant="link"
+          className="text-center text-blue-600 text-base font-semibold underline leading-normal"
+        >
+          ลงทะเบียน
+        </Button>
       </div>
     </div>
   );
