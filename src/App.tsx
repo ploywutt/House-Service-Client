3f76cd343fb1843175962814e6b53d8e1c331c20
ploywutt <ploy.wutt@gmail.com>
2023-09-05 18:28:49 +0700

    feat: resolve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/main";
 import Homepage from "./pages/Homepage";
-<<<<<<< HEAD
 import UserLogin from "./pages/LoginPage";
-=======
->>>>>>> ad48b4b140cd6cb6b282b6feb911637807c98bb4
 import Registerpage from "./pages/Registerpage";
 
 function App() {
@@ -15,10 +12,7 @@ function App() {
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<Homepage />}></Route>
-<<<<<<< HEAD
             <Route path="/login" element={<UserLogin />}></Route>
-=======
->>>>>>> ad48b4b140cd6cb6b282b6feb911637807c98bb4
             <Route path="/register" element={<Registerpage />}></Route>
           </Route>
         </Routes>
--- src/pages/Registerpage.tsx
+++ src/pages/Registerpage.tsx
@@ -24,7 +24,7 @@ const Registerpage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/api/register', formData);
+      const response = await axios.post("/api/register", formData);
       console.log(response.data);
     } catch (error) {
       console.error(error);
@@ -32,101 +32,98 @@ const Registerpage = () => {
   };
 
   return (
-    <div className="flex justify-center pt-12" >
-      <div className="w-300 h-300 pt-8 pb-8 px-12 bg-white rounded-lg border border-gray-300 flex-col justify-center items-center inline-flex"
->
-      <h2>ลงทะเบียนผู้ใช้ใหม่</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="InputStyle w-[440px] h-[72px] flex-col justify-start items-start gap-1 inline-flex" >
-          <label className=" w-[126px] h-4 text-gray-600 text-lg font-medium" htmlFor="fullName">ชื่อ-นามสกุล</label>
-          <br />
-          <input className="Rectangle7 w-[460px] h-[38px] bg-white rounded-[5px] border border-zinc-400" 
-            type="text"
-<<<<<<< HEAD
-            id="fullName" 
-            name="fullName"
-            value={formData.firstName}
-=======
-            id="fullName"
-            name="fullName"
-            value={formData.fullName}
->>>>>>> ad48b4b140cd6cb6b282b6feb911637807c98bb4
-            onChange={handleChange}
-            required
-          />
-        </div>
-        
-        <div className="Frame17 w-[460px] h-[73px] flex-col justify-center items-start gap-[19px] inline-flex">
-          <label htmlFor="phoneNumber">เบอร์โทรศัพท์</label>
-          <br />
-          <input
-            type="tel"
-            id="phoneNumber"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">อีเมลล์</label>
-          <br />
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="username">ชื่อผู้ใช้</label>
-          <br />
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">รหัสผ่าน</label>
-          <br />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <p>
-          <input
-            type="checkbox"
-            id="acceptTerms"
-            name="acceptTerms"
-            required
-          />
-          <label htmlFor="acceptTerms">
-            คุณได้ยอมรับ ข้อตกลงและเงื่อนไข และ นโยบายความเป็นส่วนตัว
-          </label>
-        </p>
+    <div className="flex justify-center pt-12">
+      <div className="w-300 h-300 pt-8 pb-8 px-12 bg-white rounded-lg border border-gray-300 flex-col justify-center items-center inline-flex">
+        <h2>ลงทะเบียนผู้ใช้ใหม่</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="InputStyle w-[440px] h-[72px] flex-col justify-start items-start gap-1 inline-flex">
+            <label
+              className=" w-[126px] h-4 text-gray-600 text-lg font-medium"
+              htmlFor="fullName"
+            >
+              ชื่อ-นามสกุล
+            </label>
+            <br />
+            <input
+              className="Rectangle7 w-[460px] h-[38px] bg-white rounded-[5px] border border-zinc-400"
+              type="text"
+              id="fullName"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <button type="submit">ลงทะเบียน</button>
-        <button type="button">เข้าสู่ระบบ</button>
-        <p>หรือ</p>
-      <button type="button">เข้าสู่ระบบด้วย Facebook</button>
-      </form>
-      <button type="button" onClick={() => history.push("/login")}>
-      กลับเข้าสู่หน้าเข้าสู่ระบบ
-      </button>
+          <div className="Frame17 w-[460px] h-[73px] flex-col justify-center items-start gap-[19px] inline-flex">
+            <label htmlFor="phoneNumber">เบอร์โทรศัพท์</label>
+            <br />
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">อีเมลล์</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="username">ชื่อผู้ใช้</label>
+            <br />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">รหัสผ่าน</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <p>
+            <input
+              type="checkbox"
+              id="acceptTerms"
+              name="acceptTerms"
+              required
+            />
+            <label htmlFor="acceptTerms">
+              คุณได้ยอมรับ ข้อตกลงและเงื่อนไข และ นโยบายความเป็นส่วนตัว
+            </label>
+          </p>
 
-      
-    </div>
+          <button type="submit">ลงทะเบียน</button>
+          <button type="button">เข้าสู่ระบบ</button>
+          <p>หรือ</p>
+          <button type="button">เข้าสู่ระบบด้วย Facebook</button>
+        </form>
+        <button type="button" onClick={() => history.push("/login")}>
+          กลับเข้าสู่หน้าเข้าสู่ระบบ
+        </button>
+      </div>
     </div>
   );
 };
@@ -134,5 +131,4 @@ const Registerpage = () => {
 //term
 //
 
-
-export default Registerpage ;
+export default Registerpage;
