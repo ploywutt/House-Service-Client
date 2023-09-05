b0ba0217fb4602d68a900c80780bff6d796c3403
rife <charift998@gmail.com>
2023-09-05 23:50:06 +0700

    feater/newerSignUp

--- src/pages/Registerpage.tsx
+++ src/pages/Registerpage.tsx
@@ -32,6 +32,7 @@ const Registerpage = () => {
   };
 
   return (
+<<<<<<< Updated upstream
     <div className="flex justify-center pt-12">
       <div className="w-300 h-300 pt-8 pb-8 px-12 bg-white rounded-lg border border-gray-300 flex-col justify-center items-center inline-flex">
         <h2>ลงทะเบียนผู้ใช้ใหม่</h2>
@@ -114,6 +115,85 @@ const Registerpage = () => {
               คุณได้ยอมรับ ข้อตกลงและเงื่อนไข และ นโยบายความเป็นส่วนตัว
             </label>
           </p>
+=======
+    <div  className="flex justify-center" >
+      <div className="Frame12 w-[600px] h-[800px] relative bg-white rounded-[5px] shadow border border-zinc-300 flex-col justify-start items-start inline-flex"
+>
+      <h2>ลงทะเบียนผู้ใช้ใหม่</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="InputStyle w-[440px] h-[72px] flex-col justify-start items-start gap-1 inline-flex" >
+          <label className=" w-[126px] h-4 text-gray-600 text-lg font-medium" htmlFor="fullName">ชื่อ-นามสกุล</label>
+          <br />
+          <input className="Rectangle7 w-[460px] h-[38px] bg-white rounded-[5px] border border-zinc-400" 
+            type="text"
+            id="fullName" 
+            name="fullName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
+        <div >
+          <label htmlFor="phoneNumber">เบอร์โทรศัพท์</label>
+          <br />
+          <input className="Rectangle7 w-[460px] h-[38px] bg-white rounded-[5px] border border-zinc-400"
+            type="tel"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">อีเมล</label>
+          <br />
+          <input  className="Rectangle7 w-[460px] h-[38px] bg-white rounded-[5px] border border-zinc-400" 
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
+        <div>
+          <label htmlFor="password">รหัสผ่าน</label>
+          <br />
+          <input  className="Rectangle7 w-[460px] h-[38px] bg-white rounded-[5px] border border-zinc-400"
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <p>
+          <input
+            type="checkbox"
+            id="acceptTerms"
+            name="acceptTerms"
+            required
+          />
+          <label htmlFor="acceptTerms">
+            ยอมรับ  <a href="#">ข้อตกลงและเงื่อนไข</a> และ <a href="#">นโยบายความเป็นส่วนตัว</a>
+          </label>
+      
+        </p>
+        
+        <button type="submit">ลงทะเบียน</button>
+        <br />
+        <button type="button" onClick={() =>("/login")}>
+      เข้าสู่ระบบ
+      </button>
+        <p>หรือ</p>
+      <button type="button">เข้าสู่ระบบด้วย Facebook</button>
+      </form>
+      
+>>>>>>> Stashed changes
 
           <button type="submit">ลงทะเบียน</button>
           <button type="button">เข้าสู่ระบบ</button>
@@ -128,7 +208,12 @@ const Registerpage = () => {
   );
 };
 
+<<<<<<< Updated upstream
 //term
 //
 
 export default Registerpage;
+=======
+
+export default Registerpage ;
+>>>>>>> Stashed changes
