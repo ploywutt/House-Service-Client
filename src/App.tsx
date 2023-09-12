cf5e58852c3842af583a3b79ae30be67634eaee5
themeyv <themeyv@gmail.com>
2023-09-12 20:54:24 +0700

    feat: add card components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/main";
 import NoFooter from "./layouts/withoutfooter";
+import ServiceFooter from "./layouts/service-footer";
 import Homepage from "./pages/Homepage";
 import Servicelistpage from "./pages/Servicelistpage";
 import LoginPage from "./pages/LoginPage";
@@ -17,12 +18,14 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Homepage />}></Route>
             <Route path="/servicelist" element={<Servicelistpage />}></Route>
-            <Route path="/service" element={<Servicedetail />}></Route>
             <Route path="/shadcn" element={<Shadcn />}></Route>
           </Route>
           <Route element={<NoFooter />}>
             <Route path="/login" element={<LoginPage />}></Route>
             <Route path="/register" element={<Registerpage />}></Route>
+          </Route>
+          <Route element={<ServiceFooter />}>
+            <Route path="/service" element={<Servicedetail />}></Route>
           </Route>
         </Routes>
       </Router>
---
+++ src/layouts/service-footer.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "@/components/layout/navbar";
+import ServiceFooterButton from "../components/layout/servicefooterbutton";
+
+function ServiceFooter() {
+  return (
+    <div className="min-h-screen">
+      <Navbar></Navbar>
+      <div className="bg-[#F3F4F6]" style={{ minHeight: "calc(100vh)" }}>
+        <Outlet />
+      </div>
+      <ServiceFooterButton />
+    </div>
+  );
+}
+
+export default ServiceFooter;
