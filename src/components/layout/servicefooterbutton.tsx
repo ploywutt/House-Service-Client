165cf5407dbd4f9128e4fddc805724fee43961de
Dos <DosDeosos@gmail.com>
2023-09-13 10:37:35 +0700

    Update Service Footer

--- src/components/layout/servicefooterbutton.tsx
+++ src/components/layout/servicefooterbutton.tsx
@@ -4,7 +4,7 @@ import ArrowRight from "../../assets/icon/arrow_right.png";
 
 function ServiceFooterButton() {
   return (
-    <div className="w-full h-92 bg-pink-300 flex justify-evenly mt-24">
+    <div className="px-52 flex justify-between py-6  bg-white">
       <Button variant="secondary" className="w-40 h-11 bg-white">
         <img src={ArrowLeft} alt="ArrowLeft" />
         <p className="ml-2">ย้อนกลับ</p>
