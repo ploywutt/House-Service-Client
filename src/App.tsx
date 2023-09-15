f98e995e17d6b756102ae71c1e3a5bb75aa2f413
Guer <maythee.srianut@gmail.com>
2023-09-15 12:46:16 +0700

    rebase: solve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,11 @@ import Servicelistpage from "./pages/Servicelistpage";
 import LoginPage from "./pages/LoginPage";
 import Registerpage from "./pages/Registerpage";
 import Shadcn from "./pages/shadecn-test";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 import ServiceDetail from "./pages/Servicedetail";
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 2f4f3b3 (rebase: solve conflict)
-import Servicedetail from "./pages/Servicedetail";
->>>>>>> b4330df (rebase: solve conflict)
 import ClientInformation from "./pages/ClientInformation";
 import TimePicker from "./components/TimePicker";
 import OrderListPage from "./pages/OrderListPage";
-
 import CheckoutPage from "./pages/CheckoutPage";
-
-=======
-import Servicedetail from "./pages/Servicedetail";
-import ClientInformation from "./pages/ClientInformation";
-import TimePicker from "./components/TimePicker";
-import OrderListPage from "./pages/OrderListPage";
->>>>>>> 2f4f3b37318cb16f4e397893b5fc582f26d9d72b
 
 function App() {
   return (
@@ -40,37 +22,18 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Homepage />}></Route>
             <Route path="/servicelist" element={<Servicelistpage />}></Route>
-            <Route path="/orders" element={<OrderListPage />}></Route>
             <Route path="/shadcn" element={<Shadcn />}></Route>
           </Route>
           <Route element={<NoFooter />}>
             <Route path="/login" element={<LoginPage />}></Route>
             <Route path="/register" element={<Registerpage />}></Route>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <Route path="/service/:id" element={<ServiceDetail />}></Route>
-=======
-<<<<<<< HEAD
->>>>>>> b4330df (rebase: solve conflict)
-=======
->>>>>>> 2f4f3b3 (rebase: solve conflict)
           </Route>
           <Route element={<ServiceFooter />}>
             <Route path="/test" element={<TimePicker />}></Route>
             <Route path="/address" element={<ClientInformation />}></Route>
-<<<<<<< HEAD
-           
-            <Route path="/checkout" element={<CheckoutPage/>}></Route>
-=======
-            <Route path="/service" element={<Servicedetail />}></Route>
-<<<<<<< HEAD
-=======
+            <Route path="/checkout" element={<CheckoutPage />}></Route>
             <Route path="/orders" element={<OrderListPage />}></Route>
-            <Route path="/shadcn" element={<Shadcn />}></Route>
->>>>>>> 3b40dd0 (feat: make orderlist page)
->>>>>>> b4330df (rebase: solve conflict)
-=======
->>>>>>> 2f4f3b3 (rebase: solve conflict)
+            <Route path="/service/:id" element={<ServiceDetail />}></Route>
           </Route>
         </Routes>
       </Router>
