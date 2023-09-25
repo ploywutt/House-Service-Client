65e687eb617fa93ecebc48457e2853934b53c37e
Guer <maythee.srianut@gmail.com>
2023-09-25 10:19:50 +0700

    feat: add axios put (ongoing)

--- src/components/profile/EditProfile.tsx
+++ src/components/profile/EditProfile.tsx
@@ -5,9 +5,66 @@ import { Label } from "../ui/label";
 import avatar from "../../assets/image/Avatar-image-profile.png";
 
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import supabase from "@/auth/supabaseauth";
+import axios from "axios";
+import useFetchUserEmail from "@/hook/useFetchUserEmail";
 
 function EditProfile(props) {
   const { t } = useTranslation();
+
+  const [file, setFile] = useState(null);
+  const [url, setUrl] = useState(avatar);
+  const [inputValues, setInputValues] = useState({});
+
+  console.log(inputValues);
+  console.log(file);
+
+  const currentUserEmail = useFetchUserEmail();
+  console.log(`edit ${currentUserEmail}`);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues({ ...inputValues, [name]: value });
+  };
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    setUrl(URL.createObjectURL(event.target.files[0]));
+    const { name } = event.target;
+    setInputValues({
+      ...inputValues,
+      [name]: `${URL.createObjectURL(event.target.files[0])}`,
+    });
+  };
+
+  const handleUpload = async () => {
+    try {
+      const { data, error } = await supabase.storage
+        .from("images")
+        .upload(file.name, file);
+      if (error) throw error;
+      console.log("File uploaded: ", data.Key);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleUpdate = async () => {
+    {
+      file && handleUpload();
+    }
+    try {
+      const response = await axios.put(
+        `http://localhost:4000/v1/user/profile?email=${currentUserEmail}`,
+        inputValues
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   console.log(props.fetchData);
   const fullName = props.fetchData[0]?.name || "";
   const phone = props.fetchData[0]?.phone || "";
@@ -17,26 +74,32 @@ function EditProfile(props) {
     {
       label: t("edit_profile_page.name_fullname"),
       placeholder: fullName,
+      varName: "name",
     },
     {
       label: t("edit_profile_page.telephone"),
       placeholder: phone,
+      varName: "phone",
     },
     {
       label: t("edit_profile_page.email"),
       placeholder: email,
+      varName: "email",
     },
     {
       label: t("edit_profile_page.current_password"),
       placeholder: "xxxxxxxx",
+      varName: "password",
     },
     {
       label: t("edit_profile_page.new_password"),
       placeholder: "xxxxxxxx",
+      varName: "newPassword",
     },
     {
       label: t("edit_profile_page.confirm_password"),
       placeholder: "xxxxxxxx",
+      varName: "reNewPassword",
     },
   ];
 
@@ -50,14 +113,20 @@ function EditProfile(props) {
         className="flex gap-24 justify-evenly items-center m-16"
       >
         <div id="avatar" className="flex flex-col gap-10 items-center">
-          <img src={avatar} alt="avatar" />
+          <img
+            src={url}
+            alt="avatar"
+            id="avatar"
+            className="w-[150px] h-[150px] rounded-full"
+          />
           <div id="upload" className="flex flex-col gap-1">
             <input
               id="file"
               type="file"
+              name="avatar_url"
               accept=".jpg, .jpeg, .png, .gif"
               className="hidden"
-              // onChange={this.handleFileUpload}
+              onChange={handleFileChange}
             />
             <label
               htmlFor="file"
@@ -76,7 +145,13 @@ function EditProfile(props) {
           {userInfo.map((item, index) => (
             <div id="input-container" key={index}>
               <Label htmlFor={item.label}>{item.label}</Label>
-              <Input id={item.label} placeholder={item.placeholder} />
+              <Input
+                id={item.varName}
+                name={item.varName}
+                placeholder={item.placeholder}
+                value={inputValues[item.varName]}
+                onChange={handleInputChange}
+              />
             </div>
           ))}
 
@@ -86,6 +161,9 @@ function EditProfile(props) {
             </Button>
             <Button className="px-2.5 py-6 w-[166px]">
               {t("edit_profile_page.save")}
+            </Button>
+            <Button className="px-2.5 py-6 w-[166px]" onClick={handleUpdate}>
+              บันทึก
             </Button>
           </div>
         </div>
