4db6544de2d66b27691ae2db9ebe27e77dabe5d8
ploywutt <ploy.wutt@gmail.com>
2023-10-02 21:27:44 +0700

    resolve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -18,52 +18,46 @@ import EditProfilePage from "./pages/EditProfilePage";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "@/components/ThemeProvider";
-<<<<<<< HEAD
 // import CheckoutForm from "./pages/CheckoutForm";
 // import { useEffect,useContext ,createContext } from "react";
 
 import { PayProvider } from "./hook/PayContext";
 // import SuccessPage from "./components/success";
-=======
-import CheckoutForm from "./pages/CheckoutForm";
-// import { useEffect,useContext ,createContext } from "react";
-
-import { PayProvider } from "./hook/PayContext";
-import SuccessPage from "./components/success";
->>>>>>> 702ff04 (stripeFix)
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <PayProvider>
-      <I18nextProvider i18n={i18n}>
-        <Router>
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<Homepage />}></Route>
-              <Route path="/servicelist" element={<Servicelistpage />}></Route>
-              <Route path="/shadcn" element={<Shadcn />}></Route>
-              <Route path="/orders" element={<OrderListPage />}></Route>
-              <Route path="/history" element={<HistoryPage />}></Route>
-              <Route path="/profile" element={<ProfilePage />}></Route>
-              <Route path="/edit-profile" element={<EditProfilePage />}></Route>
-            </Route>
-            <Route element={<NoFooter />}>
-              <Route path="/login" element={<LoginPage />}></Route>
-              <Route path="/register" element={<Registerpage />}></Route>   
-              <Route path="/service/:id" element={<ServiceDetail />}></Route>
-<<<<<<< HEAD
-              {/* <Route path="/success" element={<SuccessPage />}></Route> */}
-=======
-              <Route path="/success" element={<SuccessPage />}></Route>
->>>>>>> 702ff04 (stripeFix)
-            </Route>
-            <Route element={<ServiceFooter />}>
-              {/* <Route path="/checkout" element={<CheckoutForm/>}></Route> */}
-            </Route>
-          </Routes>
-        </Router>
-      </I18nextProvider>
+        <I18nextProvider i18n={i18n}>
+          <Router>
+            <Routes>
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Homepage />}></Route>
+                <Route
+                  path="/servicelist"
+                  element={<Servicelistpage />}
+                ></Route>
+                <Route path="/shadcn" element={<Shadcn />}></Route>
+                <Route path="/orders" element={<OrderListPage />}></Route>
+                <Route path="/history" element={<HistoryPage />}></Route>
+                <Route path="/profile" element={<ProfilePage />}></Route>
+                <Route
+                  path="/edit-profile"
+                  element={<EditProfilePage />}
+                ></Route>
+              </Route>
+              <Route element={<NoFooter />}>
+                <Route path="/login" element={<LoginPage />}></Route>
+                <Route path="/register" element={<Registerpage />}></Route>
+                <Route path="/service/:id" element={<ServiceDetail />}></Route>
+                {/* <Route path="/success" element={<SuccessPage />}></Route> */}
+              </Route>
+              <Route element={<ServiceFooter />}>
+                {/* <Route path="/checkout" element={<CheckoutForm/>}></Route> */}
+              </Route>
+            </Routes>
+          </Router>
+        </I18nextProvider>
       </PayProvider>
     </ThemeProvider>
   );
