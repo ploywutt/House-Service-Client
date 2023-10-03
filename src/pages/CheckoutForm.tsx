b778792f0da2a3e84c056a89f6db4c0e159413d6
ploywutt <ploy.wutt@gmail.com>
2023-10-03 11:05:57 +0700

    hotfix: add-coupon

--- src/pages/CheckoutForm.tsx
+++ src/pages/CheckoutForm.tsx
@@ -4,14 +4,14 @@ import { Elements } from "@stripe/react-stripe-js";
 import PaymentForm from "./PaymentForm";
 import useClientSecretStripe from "@/hook/useClientsecretStripe";
 
-export default function CheckoutForm({ totalprice }) {
+export default function CheckoutForm({ totalprice, orderTotalPrice }) {
   const stripePromise = loadStripe(
     "pk_test_51NozskHa6CHfGgr1Mlek2lwtRJjpDwWxNA0gn2HOsVJpCHvdw8IU3SC49hc4w38V8tAW8i3AexxQD7PJ9JACmlt800wDbJcXNt"
   );
   const { clientSecret, createPaymentIntent } = useClientSecretStripe();
 
   useEffect(() => {
-    createPaymentIntent(totalprice);
+    createPaymentIntent(totalprice, orderTotalPrice);
   }, []);
 
   const appearance = {
